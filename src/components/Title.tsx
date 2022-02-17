e8d44295ef5bcca22eaba2d9f80585e99374fc18
valeria-ab <lera.magnum@gmail.com>
2022-02-17 12:31:24 +0300

    first commit

---
+++ src/components/Title.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    const data = {
+        checkbox: false,
+        header: '',
+        body: ''
+    }
+    const onClick = () => {
+
+    }
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default App;
